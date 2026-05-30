5def75d232dcaa8f2d24a6a9bd8035dc20e25ac6
Leonardo Telleria <telleriatrujilloleonardoanti@gmail.com>
2026-05-30 16:07:57 -0600

    modificando el mappingservice

--- src/modules/mapping/mappingService.ts
+++ src/modules/mapping/mappingService.ts
@@ -1,11 +1,17 @@
-import {SimulationData} from './mappingTypes';
-import roverSimulationMock from '../../data/roverSimulationMock.json';
+import fs from 'fs';
+import path from 'path';
 
 
 //nueva clase xd
 export class SimulationService {
     //obteniendo el json
-    getSimulation(): SimulationData {
-        return roverSimulationMock as unknown as SimulationData;
+    getSimulation() {
+        const mockPath = path.join(__dirname,'../data/roverSimulationMock.json');
+        const fileContent = fs.readFileSync(mockPath, 'utf-8');
+        const rawData = JSON.parse(fileContent);//convierte el text en json
+        return {//retorna un objeto con los datos timestamp y stepindex
+            ...rawData,
+            stepIndex: 0
+        };
     }
 }
