bd3288fce3fa497799b8aee1650aa1163cb9ed20
luis-hdz7 <luisjorgeantonhernandez@gmail.com>
2026-05-30 20:03:48 -0600

    Pequeños avances para el dia 3

---
+++ src/services/eventDetection.ts
@@ -0,0 +1,90 @@
+import type {
+  Plant,
+  Obstacle,
+  RoverPosition,
+  SimulationEvent,
+} from "../modules/mapping/mappingTypes";
+
+function calculateDistance(
+  x1: number,
+  y1: number,
+  x2: number,
+  y2: number
+): number {
+  return Math.sqrt(
+    (x2 - x1) ** 2 +
+    (y2 - y1) ** 2
+  );
+}
+
+const DETECTION_RADIUS = 5;
+
+export function detectEvents(
+  trajectory: RoverPosition[],
+  plants: Plant[],
+  obstacles: Obstacle[]
+): SimulationEvent[] {
+
+  const events: SimulationEvent[] = [];
+
+  const detectedPlants = new Set<string>();
+  const detectedObstacles = new Set<string>();
+
+  trajectory.forEach((point) => {
+
+    plants.forEach((plant) => {
+
+      const distance = calculateDistance(
+        point.x,
+        point.y,
+        plant.x,
+        plant.y
+      );
+
+      if (
+        distance <= DETECTION_RADIUS &&
+        !detectedPlants.has(plant.id)
+      ) {
+
+        detectedPlants.add(plant.id);
+
+        events.push({
+          message: "Plant detected",
+          timestamp: point.timestamp,
+          plantId: plant.id,
+          x: point.x,
+          y: point.y
+        });
+      }
+    });
+
+    obstacles.forEach((obstacle) => {
+
+      const distance = calculateDistance(
+        point.x,
+        point.y,
+        obstacle.x,
+        obstacle.y
+      );
+
+      if (
+        distance <= DETECTION_RADIUS &&
+        !detectedObstacles.has(obstacle.id)
+      ) {
+
+        detectedObstacles.add(obstacle.id);
+
+        events.push({
+          message: "Obstacle detected",
+          timestamp: point.timestamp,
+          obstacleId: obstacle.id,
+          x: point.x,
+          y: point.y
+        });
+      }
+    });
+
+  });
+
+  return events;
+}
