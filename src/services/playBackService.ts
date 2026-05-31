e0c2bda201d744b9dd07ae372dec63d8325c850b
OsvaldoZzz <elhh1985@gmail.com>
2026-05-31 16:34:58 -0600

    Se cambio el (playback) para que devuelva un solo playback

--- src/services/playBackService.ts
+++ src/services/playBackService.ts
@@ -4,21 +4,22 @@ import type {
   SimulationEvent,
 } from "../modules/mapping/mappingTypes";
 
+//se cambio PlayBackStep[] para devolver 1 playback unico
+
 export function buildPlayback(
   trajectory: RoverPosition[],
   events: SimulationEvent[]
-): PlaybackStep[] {
-
-  return trajectory.map((point, index) => ({
+) {
+  const frames = trajectory.map((point, index) => ({
     step: index + 1,
-
     rover: {
       x: point.x,
       y: point.y,
     },
-
-    events: events.filter(
-      event => event.step === index + 1
-    ),
+    events: events.filter(event => event.step === index + 1),
   }));
+
+  return {
+    frames
+  };
 }
