ff7a6b319e670d60d53e5e52066a50ec60ce69d6
Brando-1510 <brandonbrt2020@gmail.com>
2026-06-03 22:45:46 -0600

    Delete src directory

--- src/modules/mapping/mappingService.ts
+++
@@ -1,8 +0,0 @@
-import { RawSimulationData } from "./mappingTypes";
-import roverSimulationMock from "../../data/roverSimulationMock.json";
-
-export class SimulationService {
-  getSimulation(): RawSimulationData {
-    return roverSimulationMock as RawSimulationData;
-  }
-}
