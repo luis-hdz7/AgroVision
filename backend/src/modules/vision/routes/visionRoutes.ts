eaa81cc6517cc601871df262db7bd43f25b26e3d
Leonardo Telleria <telleriatrujilloleonardoanti@gmail.com>
2026-07-22 12:12:49 -0600

    montando ruta

---
+++ backend/src/modules/vision/routes/visionRoutes.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { VisionController } from "../controllers/visionController";
+
+const router = Router();
+/**
+ * POST /api/vision/analyze
+ * Recibe una solicitud de análisis visual y devuelve
+ * una predicción compatible con el AI Service.
+ */
+router.get("/analyze", VisionController.analyzeImage);
+
+export default router;
